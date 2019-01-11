0a2321e82e9d7e5d8a8e39920c8013ff75f63cca
Amit <amrx101@gmail.com>
2019-01-11 19:58:21 +0530

    Added react native element

--- components/deckList.js
+++ components/deckList.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const list = ["Number1", "Number2", "Number3"]
+
+let deck1 = {
+    name: 'Deck1',
+    Questions: 3,
+}
+
+let deck2 = {
+    name: 'Deck2',
+    Questions: 2
+}
+
+let Decks = [deck1, deck2]
 
 class DeckList extends React.Component{
     switchToDeck = (deck) => {
          this.props.navigation.navigate('Deck')
+    }
+
+    renderRow = ({deck}) => {
+        
+
     }
     render(){
         return(
