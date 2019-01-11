b2c835e048076043732db691e66a3d052a095253
Amit <amrx101@gmail.com>
2019-01-11 20:33:33 +0530

    WIP

--- components/deckList.js
+++ components/deckList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
 
 
 let deck1 = {
@@ -15,21 +16,55 @@ let deck2 = {
 let Decks = [deck1, deck2]
 
 class DeckList extends React.Component{
+
+    state = {
+        decks: Decks,
+    }
     switchToDeck = (deck) => {
          this.props.navigation.navigate('Deck')
     }
 
     renderRow = ({deck}) => {
-        
-
+        console.log("Deck", deck);
+        <ListItem>
+            title={deck.name}
+            subtitle={deck.Questions}
+        </ListItem>
     }
+
     render(){
         return(
-            <View>
-                <Text> This is it</Text>
-            </View>
+            <List>
+                <FlatList>
+                    data={this.state.decks},
+                    renderItem={this.renderRow},
+                    keyExtractor={deck => deck.name},
+                </FlatList>
+            </List>
         )
     }
+    
 }
+
+/*
+render() {
+    return (
+      <ScrollView>
+        <List>
+          {decks.map((deck) => (
+            <ListItem
+              key={deck.name}
+              roundAvatar
+              avatar={{ uri: user.picture.thumbnail }}
+              title={`${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}`}
+              subtitle={user.email}
+              onPress={() => this.onLearnMore(user)}
+            />
+          ))}
+        </List>
+      </ScrollView>
+    );
+  }
+*/ 
 
 export default DeckList
