4c2af881bb916427af19bf7ab13d6e8db64992d3
Amit <amrx101@gmail.com>
2019-01-11 22:09:47 +0530

    DeckList Scroll View

--- components/deckList.js
+++ components/deckList.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, ScrollView } from 'react-native';
 import { List, ListItem } from 'react-native-elements'
 
 
-let deck1 = {
+const deck1 = {
     name: 'Deck1',
     Questions: 3,
 }
 
-let deck2 = {
+const deck2 = {
     name: 'Deck2',
     Questions: 2
 }
 
-let Decks = [deck1, deck2]
+const Decks = [deck1, deck2]
 
 class DeckList extends React.Component{
 
-    state = {
-        decks: Decks,
-    }
     switchToDeck = (deck) => {
          this.props.navigation.navigate('Deck')
     }
@@ -32,39 +29,23 @@ class DeckList extends React.Component{
         </ListItem>
     }
 
-    render(){
-        return(
-            <List>
-                <FlatList>
-                    data={this.state.decks},
-                    renderItem={this.renderRow},
-                    keyExtractor={deck => deck.name},
-                </FlatList>
-            </List>
+    render() {
+        return (
+            <ScrollView>
+                <List>
+                {Decks.map((deck) => (
+                    <ListItem
+                    key={deck.name}
+                    title={deck.name}
+                    subtitle={deck.Questions}
+                    />
+                ))}
+                </List>
+            </ScrollView>
         )
     }
-    
 }
 
-/*
-render() {
-    return (
-      <ScrollView>
-        <List>
-          {decks.map((deck) => (
-            <ListItem
-              key={deck.name}
-              roundAvatar
-              avatar={{ uri: user.picture.thumbnail }}
-              title={`${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}`}
-              subtitle={user.email}
-              onPress={() => this.onLearnMore(user)}
-            />
-          ))}
-        </List>
-      </ScrollView>
-    );
-  }
-*/ 
+
 
 export default DeckList
