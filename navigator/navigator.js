706b67b9f5860909a02573d57417af077a205b08
Amit <amrx101@gmail.com>
2019-01-11 19:31:32 +0530

    Tab nav icons, wip

--- navigator/navigator.js
+++ navigator/navigator.js
@@ -3,11 +3,24 @@ import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
 import DeckList from '../components/deckList';
 import AddDeck from '../components/addDeck';
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
 
 const Tabs = createBottomTabNavigator({
-  Decks: {screen: DeckList},
-  NewDeck: {screen: AddDeck},
+  Decks: {
+    screen: DeckList, 
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='th-list' size={30} color={tintColor} />
+    },
+  },
+  NewDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'NewDeck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  },
 });
 
 const TabNavigator = createAppContainer(Tabs);
