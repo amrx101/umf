55eefd0264c158f9c7693b7736ee929fce35e6a0
Amit <amrx101@gmail.com>
2019-01-12 16:21:15 +0530

    Added form for new deck

--- components/addDeck.js
+++ components/addDeck.js
@@ -1,12 +1,41 @@
 import React   from 'react'
 import {Text, View} from 'react-native'
+import { FormLabel, FormInput } from 'react-native-elements'
 
 class AddDeck extends React.Component {
+    state = {
+        name: ''
+    }
+
+    setName = (text) => {
+        this.setState({name: text})
+    }
+
+    createDeck = (e) => {
+        // How do we dynamically add navigation routes?
+        console.log("Create New Deck and route to this deck")
+    }
+
+    renderForm = (label, callable, field) => {
+        return(
+            <View>
+                <FormLabel>{label}</FormLabel>
+                <FormInput
+                    onChangeText={callable}
+                    value={field}
+                />
+            </View>
+        )
+    }
+
+    
+
+
     render(){
         return(
             <View>
                 <Text> This is AddDeck Page. </Text>
-                <Text> Please review rubrics and the video, if this page has to be the second navigation </Text>
+                {this.renderForm("Name of the Deck", this.setName, this.state.name)}
             </View>
         )
     }
