940cc98e4bce7cfb733f573b0a2aefcb000a608e
Amit <amrx101@gmail.com>
2019-01-12 21:29:18 +0530

    Wip

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TabNavigator from './navigator/navigator'
+import {createStore} from 'redux'
+
+import { reducer } from '../reducers/index'
+const umfStore = createStore(reducer)
 
 export default class App extends React.Component {
   render() {
