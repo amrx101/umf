d109b09c0169c79b00a85b70ef4fe6933c8f6437
Amit <amrx101@gmail.com>
2019-01-16 22:07:31 +0530

    Refactored button component

---
+++ components/common.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Button } from 'react-native-elements';
+import {View, StyleSheet} from 'react-native';
+
+class CustomButton extends React.Cumponent {
+    
+    render(){
+        const {styleIdentifier} = this.props
+        let style = styles.default
+        if (styleIdentifier === "red") {
+            buttonStyle = styles.red
+        } else if (styleIdentifier === "green"){
+            buttonStyle = styles.green
+        }
+
+        return(
+            <View style={{padding:10}}>
+                <Button
+                    title={name}
+                    onPress={callable}
+                    buttonStyle={buttonStyle}
+                    containerStyle={{ marginBottom: 20, }}
+                />
+            </View>              
+        )
+    }  
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20,
+    },
+    green: {
+        backgroundColor: "green",
+        width: 300,
+        height: 45,
+        borderColor: "transparent",
+        borderWidth: 0,
+        borderRadius: 5,
+        padding: 10
+    },
+    red: {
+        backgroundColor: "red",
+        width: 300,
+        height: 45,
+        borderColor: "transparent",
+        borderWidth: 0,
+        borderRadius: 5,
+        padding: 10
+    },
+    default: {
+        backgroundColor: "rgba(92, 99,216, 1)",
+        width: 300,
+        height: 45,
+        borderColor: "transparent",
+        borderWidth: 9,
+        borderRadius: 5,
+        padding: 10,
+    }
+});
